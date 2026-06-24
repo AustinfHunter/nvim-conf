c467baeb15a56a87c80c364d3d116b992e4c9336
Austin Hunter <ahunter@skybite.net>
2026-06-24 10:35:07 -0400

    update configs

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,15 +1,33 @@
-local lspconfig = require('lspconfig')
-lspconfig.pyright.setup({})
-lspconfig.gopls.setup({
-	cmd = {"gopls"},
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      staticcheck = true,
-      gofumpt = true,
-    },
-  },
-})
-lspconfig.wgsl_analyzer.setup({})
+return {
+	vim.lsp.config('pyright', {}),
+	vim.lsp.config('gopls',{
+		cmd = {"gopls"},
+		settings = {
+			gopls = {
+				analyses = {
+					unusedparams = true,
+				},
+				staticcheck = true,
+				gofumpt = true,
+			},
+		},
+	}),
+	vim.lsp.config('wgsl_analyzer', {}),
+	vim.lsp.config('prismals',{}),
+	vim.lsp.config('html',{
+		filetypes = {"html", "mjml"},
+	}),
+	vim.lsp.config('ts_ls', { 
+		preferences = {
+			includeCompletionsForModuleExports = true,
+			includeCompletionsForImportStatements = true,
+			importModuleSpecifier = 'relative',
+		},
+	}),
+	vim.lsp.enable('pyright'),
+	vim.lsp.enable('gopls'),
+	vim.lsp.enable('wgsl_analyzer'),
+	vim.lsp.enable('prismals'),
+	vim.lsp.enable('html'),
+	vim.lsp.enable('ts_ls'),
+}
